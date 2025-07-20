d4ea5d54f336b5751c3adde86b5b9c91c849f305
TonyRodIv <antony.rodriguesiv@gmail.com>
2025-07-20 20:08:41 -0300

    Atualiza título da página para "Previsão do Tempo"

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import LocalWeather from './components/LocalWeather';
 function App() {
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1>Meu App de Previsão do Tempo</h1>
+      <h1>Previsão do Tempo</h1>
       
       <section>
         <h3>Tempo na sua Localização Atual</h3>
